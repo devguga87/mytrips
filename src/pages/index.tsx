7961553b37abfeec5bf903d1d6e37cb25b788217
Gustavo Seabra <devguga87@gmail.com>
2021-04-14 23:01:46 -0300

    feature:adding map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import Map from 'components/Map'
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return (
-    <>
-      <h1>Hello</h1>
-      <Map />
-    </>
-  )
+  return <Map />
 }
